839ebd8656fbaa1128f75b9118585672abd3ecea
ARYAN JAIN <117700812+gitsofaryan@users.noreply.github.com>
2023-05-26 12:30:04 +0530

    Add files via upload

---
+++ Day2.py
@@ -0,0 +1,63 @@
+# lst_num = [1, 2, 3, 4, 5, 6]
+# lst_num2 = [1, 2, 3, 4, 5, 6, 'hello', 'python']
+# lst_num3 = [1, 2, 3, 4, 5, 6, 'hello', 'python',[1.0,2.0,3.0]]
+
+# print(lst_num)
+# print(lst_num3)
+# print(type(lst_num2))
+# print(len(lst_num2))
+# print(len(lst_num3[7]))
+# print(lst_num3[2:4])
+
+# lst_num2.append('77')
+# print(lst_num2)
+
+# ---------------------------------------------------------------------------------------------------------------
+
+
+# dict_num1 ={1:'one',2:'two',3:'three'}
+# print(dict_num1[2])
+
+# dict_num2 = {29: {'maths':44}, 78:{'physics':100}, 23:{'chem':74},34:{'sst':100}}
+# print(dict_num2[78]['physics'])
+
+
+# ---------------------------------------------------------------------------------------------------------------
+
+
+# sets are  mutable
+
+# set_num1={1,2,3,5,6,7}
+# set_num2 = {1, 2, 3, 5,76, 97}
+
+# print(set_num2.difference(set_num1))
+# print(set_num2.intersection(set_num1))
+
+
+# list,sets,dictionary.items are mutable
+# strings,tuple,dictionary.keys are immutable
+
+
+# str_name = 'python'
+# print(len(str_name))
+# print(len('hello'))
+
+
+
+# -----------------------------------#
+
+# OPERATORS
+# x=[1,2,3,4,5]
+# y=x[:]
+# print(x==y)
+# print(x is y)
+# print(bool(x))non zero value gives true
+# print(y)
+
+# x_str='hello'
+# y = 'h' in x_str.upper()
+# print(y)
+
+# x,y =6,5
+# print(x>0 and y>0)
+
---
+++ day1.py
@@ -0,0 +1,54 @@
+# print
+# print("hello world")
+
+# high level
+# object oriented
+# dynamically typed
+# interpreted lang
+
+
+# x = 10
+# if x > 5:
+#     print("greater than 5")
+
+# if x < 5:
+#     print("lesser than 5")
+
+# x = 'python'
+# x = 10
+
+# creation of object
+# print(type(x)) //type shows the data type of variable
+
+# upper case letters
+# x.upper()
+
+
+# id of x = 10
+# id(x)
+
+# everything is a class
+
+# dir = function and feactures of a particular data type
+
+# x=10
+# x.denominator
+# help(x.denominator)
+
+# area of rectangle
+# l=5
+# y=7
+# print('area',l*y)
+
+
+# help() for getting documentation of the conjugatre
+
+# x = 'Hello'
+
+# print(x[0])
+
+# concatenation
+# y = x + ' python'
+
+# print(y[1:5])//slicing
+
